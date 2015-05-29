08ecc810b9af8f8891ca7c8b562de9f43805f88d
Dinh Quang Trung <trungdq88@gmail.com>
2015-05-29 23:41:03 +0700

    Add main activity.

--- Source code/CheckerMobileApp/app/src/main/java/com/fpt/mic/mobile/checker/app/MainActivity.java
+++ Source code/CheckerMobileApp/app/src/main/java/com/fpt/mic/mobile/checker/app/MainActivity.java
@@ -1,10 +1,12 @@
 package com.fpt.mic.mobile.checker.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -13,6 +15,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btnCheck).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
