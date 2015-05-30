70b8e4aabcee4f88943d1af208a7b9bf19affc33
Dinh Quang Trung <trungdq88@gmail.com>
2015-05-30 08:08:36 +0700

    Add InfoActivity

---
+++ Source code/CheckerMobileApp/app/src/main/java/com/fpt/mic/mobile/checker/app/InfoActivity.java
@@ -0,0 +1,15 @@
+package com.fpt.mic.mobile.checker.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dinhquangtrung on 5/29/15.
+ */
+public class InfoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
