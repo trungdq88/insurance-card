b8d73850e60f595d06e1897756e0621ce0c2a44f
minhtridn2001 <minhtridn2001@gmail.com>
2015-06-05 21:44:47 +0700

    Initial ContractDao, CustomerDao, PaymentDao.

---
+++ Source code/MICWeb/src/main/java/com/fpt/mic/micweb/model/dao/CustomerDao.java
@@ -0,0 +1,10 @@
+package com.fpt.mic.micweb.model.dao;
+
+import com.fpt.mic.micweb.model.entity.CustomerEntity;
+
+/**
+ * Created by TriPQMSE60746 on 06/05/2015.
+ */
+public class CustomerDao extends GenericDaoJpaImpl<CustomerEntity, String> {
+
+}
