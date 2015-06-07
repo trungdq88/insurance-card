795846c1beb12d1ae24079d155a7346051e191fd
Dinh Quang Trung <trungdq88@gmail.com>
2015-06-08 00:08:34 +0700

    Implement API updateCardID

--- Source code/MICWeb/src/main/java/com/fpt/mic/micweb/model/business/ApiBusiness.java
+++ Source code/MICWeb/src/main/java/com/fpt/mic/micweb/model/business/ApiBusiness.java
@@ -1,9 +1,14 @@
 package com.fpt.mic.micweb.model.business;
 
+import com.fpt.mic.micweb.model.dao.CardDao;
 import com.fpt.mic.micweb.model.dao.ContractDao;
 import com.fpt.mic.micweb.model.dto.ContractSearchResult;
+import com.fpt.mic.micweb.model.entity.CardEntity;
 import com.fpt.mic.micweb.model.entity.ContractEntity;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,5 +24,22 @@ public class ApiBusiness {
     public List<ContractSearchResult> searchContracts(String keyword) {
         ContractDao contractDao = new ContractDao();
         return contractDao.searchContracts(keyword);
+    }
+
+    public boolean updateCardID(String contractCode, String cardID) {
+        CardDao cardDao = new CardDao();
+
+        // Validate contract code, card ID
+
+
+        // Insert to database
+        CardEntity cardEntity = new CardEntity();
+        cardEntity.setContractCode(contractCode);
+        cardEntity.setCardId(cardID);
+        cardEntity.setActivatedDate(new Timestamp(new Date().getTime()));
+
+        CardEntity result = cardDao.create(cardEntity);
+
+        return result != null;
     }
 }
