650d9e5e8298f4696c65b4c999d89c5b4b873245
Dinh Quang Trung <trungdq88@gmail.com>
2015-06-09 11:39:11 +0700

    Add TerminateRequest response.

---
+++ Source code/MICWeb/src/main/java/com/fpt/mic/micweb/framework/responses/TerminateRequest.java
@@ -0,0 +1,8 @@
+package com.fpt.mic.micweb.framework.responses;
+
+/**
+ * FPT University - Capstone Project - Summer 2015 - MICWeb
+ * Created by dinhquangtrung on 6/9/15.
+ */
+public class TerminateRequest extends ResponseObject {
+}
