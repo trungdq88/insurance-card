934f063813d0eaeb897914c1ad2fc9eebeb0ae9a
Dinh Quang Trung <trungdq88@gmail.com>
2015-06-10 16:47:47 +0700

    Implement API for printer app.

---
+++ Source code/MICWeb/src/main/java/com/fpt/mic/micweb/utils/Constrants.java
@@ -0,0 +1,16 @@
+package com.fpt.mic.micweb.utils;
+
+/**
+ * FPT University - Capstone Project - Summer 2015 - MICWeb
+ * Created by dinhquangtrung on 6/10/15.
+ */
+public class Constrants {
+    public static class ContractStatus {
+        public static final String PENDING = "Pending";
+        public static final String NO_CARD = "No card";
+        public static final String READY = "Ready";
+        public static final String EXPIRED = "Expired";
+        public static final String REQUEST_CANCEL = "Request cancel";
+        public static final String CANCELLED = "Cancelled";
+    }
+}
