3e1e4ec11f34d80af718364ab49ebe39b2666cce
Kha Nguyen Chi <hydrangea8604@gmail.com>
2015-06-22 13:04:51 +0700

    Update new contract price per year bases on Vietnam Laws (added tax)

--- Database/Dummy data/1 contract type.sql
+++ Database/Dummy data/1 contract type.sql
@@ -4,8 +4,8 @@ LIMIT 0, 1000
 
 -- Date: 2015-06-05 20:57
 */
-INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (1,'Xe trên 50cc có BH cho người trên xe','Xe trên 50cc có BH cho người trên xe',85000);
-INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (2,'Xe trên 50cc không có BH cho người trên xe','Xe trên 50cc không có BH cho người trên xe',75000);
-INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (3,'Xe dưới 50cc có BH cho người trên xe','Xe dưới 50cc có BH cho người trên xe',65000);
-INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (4,'Xe dưới 50cc không có BH cho người trên xe','Xe dưới 50cc không có BH cho người trên xe',55000);
-INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (5,'Xe mô tô ba bánh, xe gắn máy và các loại xe tương tự','Bảo hiểm dành cho xe ba bánh, xe gắn máy v.v',105000);
+INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (1,'Xe trên 50cc có BH cho người trên xe','Xe trên 50cc có BH cho người trên xe',86000);
+INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (2,'Xe trên 50cc không có BH cho người trên xe','Xe trên 50cc không có BH cho người trên xe',66000);
+INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (3,'Xe từ 50cc trở xuống có BH cho người trên xe','Xe dưới 50cc có BH cho người trên xe',80500);
+INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (4,'Xe từ 50cc trở xuống có BH cho người trên xe','Xe dưới 50cc không có BH cho người trên xe',60500);
+INSERT INTO `mic_contract_type` (`id`,`name`,`description`,`price_per_year`) VALUES (5,'Xe mô tô ba bánh, xe gắn máy và các loại xe tương tự','Bảo hiểm dành cho xe ba bánh, xe gắn máy v.v',319000);
