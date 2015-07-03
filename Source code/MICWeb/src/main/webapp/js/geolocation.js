411ccd0438860e3fa821aa0114e7e9f8b3c65515
Kha Nguyen Chi <hydrangea8604@gmail.com>
2015-07-03 17:58:43 +0700

    #233 Implements create new compensation (validate client & server-side)

--- Source code/MICWeb/src/main/webapp/js/geolocation.js
+++ Source code/MICWeb/src/main/webapp/js/geolocation.js
@@ -15,6 +15,33 @@ function initialize() {
 
 }
 
+function initialize1() {
+    // Create the autocomplete object, restricting the search
+    // to geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {HTMLInputElement} */(document.getElementById('driverAddress')),
+        {types: ['geocode'], componentRestrictions: {country: 'vn'}});
+
+}
+
+function initialize2() {
+    // Create the autocomplete object, restricting the search
+    // to geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {HTMLInputElement} */(document.getElementById('accidentPlace')),
+        {types: ['geocode'], componentRestrictions: {country: 'vn'}});
+
+}
+
+function initialize3() {
+    // Create the autocomplete object, restricting the search
+    // to geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {HTMLInputElement} */(document.getElementById('observerAddress')),
+        {types: ['geocode'], componentRestrictions: {country: 'vn'}});
+
+}
+
 // [START region_geolocation]
 // Bias the autocomplete object to the user's geographical location,
 // as supplied by the browser's 'navigator.geolocation' object.
