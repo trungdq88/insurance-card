39e5a4255ac5ac223dc7b5c1d9fec95a2ab65f81
Dinh Quang Trung <trungdq88@gmail.com>
2015-07-05 17:43:44 +0700

    #259 Implement resolve new card request if has one.

--- Source code/MICWeb/src/main/java/com/fpt/mic/micweb/model/business/ApiBusiness.java
+++ Source code/MICWeb/src/main/java/com/fpt/mic/micweb/model/business/ApiBusiness.java
@@ -2,9 +2,11 @@ package com.fpt.mic.micweb.model.business;
 
 import com.fpt.mic.micweb.model.dao.CardDao;
 import com.fpt.mic.micweb.model.dao.ContractDao;
+import com.fpt.mic.micweb.model.dao.helper.NewCardRequestDao;
 import com.fpt.mic.micweb.model.dto.ContractSearchResultDto;
 import com.fpt.mic.micweb.model.entity.CardEntity;
 import com.fpt.mic.micweb.model.entity.ContractEntity;
+import com.fpt.mic.micweb.model.entity.NewCardRequestEntity;
 import com.fpt.mic.micweb.utils.Constants;
 
 import java.sql.Timestamp;
@@ -29,6 +31,7 @@ public class ApiBusiness {
     public CardEntity updateCardID(String contractCode, String cardID) {
         CardDao cardDao = new CardDao();
         ContractDao contractDao = new ContractDao();
+        NewCardRequestDao newCardRequestDao = new NewCardRequestDao();
 
         // Validate contract code, card ID
         // Basic validate if the card is already in the system
@@ -37,10 +40,26 @@ public class ApiBusiness {
             return null; // TODO: return meaningful value here.
         }
 
+        // New Card Request ID (if present)
+        Integer requestId = null;
 
         // If there is a card assigned to the contract before, deactivate it
         CardEntity oldCard = cardDao.getCardByContract(contractCode);
         if (oldCard != null) {
+
+            // If there is a new card request for the old card, resolve it.
+            NewCardRequestEntity request = newCardRequestDao.getUnsolvedRequestByCardId(oldCard.getCardId());
+            if (request != null) {
+                // Resolve the request
+                request.setResolveDate(new Timestamp(new Date().getTime()));
+                newCardRequestDao.update(request);
+                // Set request ID to the card
+                requestId = request.getId();
+
+                // TODO: Notify?
+            }
+
+
             oldCard.setDeactivatedDate(new Timestamp((new Date()).getTime()));
             cardDao.update(oldCard);
         }
@@ -50,6 +69,7 @@ public class ApiBusiness {
         cardEntity.setContractCode(contractCode);
         cardEntity.setCardId(cardID);
         cardEntity.setActivatedDate(new Timestamp(new Date().getTime()));
+        cardEntity.setNewCardRequestId(requestId);
 
         CardEntity result = cardDao.create(cardEntity);
 
