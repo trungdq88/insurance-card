d9a555a88e8915b51b8a1cbcf279a61e99063c11
Kha Nguyen Chi <hydrangea8604@gmail.com>
2015-07-09 12:17:50 +0700

    Edit dummy database, document and defense question after meeting minutes Thursday, 9th July, 2015

--- Database/Dummy data/3 staff.sql
+++ Database/Dummy data/3 staff.sql
@@ -4,11 +4,11 @@ LIMIT 0, 1000
 
 -- Date: 2015-06-05 20:58
 */
-INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV8604','123456','Nguyễn Chí Kha','khanc@gmail.com','0912025544');
-INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV0C03','123456','Lê Xuân Tiến','globavina.hrd@gmail.com','0983660909');
-INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV44C2','huyhoang','Trương Đắc Huy Hoàng','hoangtdh@gmail.com','0912116730');
-INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV44DD','minhcaoto','Vũ Nhật Minh','minhnm@gmail.com','0915250309');
-INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV4869','aptx4869','Lê Duy Thanh','tmnthanh0125@yahoo.com','0913002087');
-INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NVACDD','luangquang','Trần Đình Quang','quangtd@outlook.com','0912632096');
-INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NVDC32','tienxuanle','Lê Xuân Tiến','conan_3137@yahoo.com','0940984464');
-INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NVESC2','dongphuongminhchau','Võ Thị Minh Châu','chauvtm@gmail.com','0915020617');
+INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV8604','4297f44b13955235245b2497399d7a93','Nguyễn Chí Kha','khanc@gmail.com','0912025544');
+INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV0C03','4297f44b13955235245b2497399d7a93','Lê Xuân Tiến','globavina.hrd@gmail.com','0983660909');
+INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV44C2','4297f44b13955235245b2497399d7a93','Trương Đắc Huy Hoàng','hoangtdh@gmail.com','0912116730');
+INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV44DD','4297f44b13955235245b2497399d7a93','Vũ Nhật Minh','minhnm@gmail.com','0915250309');
+INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NV4869','4297f44b13955235245b2497399d7a93','Lê Duy Thanh','tmnthanh0125@yahoo.com','0913002087');
+INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NVACDD','4297f44b13955235245b2497399d7a93','Trần Đình Quang','quangtd@outlook.com','0912632096');
+INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NVDC32','4297f44b13955235245b2497399d7a93','Lê Xuân Tiến','conan_3137@yahoo.com','0940984464');
+INSERT INTO `mic_staff` (`staff_code`,`password`,`name`,`email`,`phone`) VALUES ('NVESC2','4297f44b13955235245b2497399d7a93','Võ Thị Minh Châu','chauvtm@gmail.com','0915020617');
