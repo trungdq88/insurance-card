f2965346953466cec32a72dfc2265a311920c90e
Kha Nguyen Chi <hydrangea8604@gmail.com>
2015-07-14 10:20:01 +0700

    Update dummy access log for test. Delete useless jsp files

--- Database/Dummy data/8 card access log.sql
+++ Database/Dummy data/8 card access log.sql
@@ -4,13 +4,13 @@ LIMIT 0, 1000
 
 -- Date: 2015-06-05 20:56
 */
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (1,'2015-03-04 00:00:00','DV001','Verify card','Valid Card','0D-3B-D1-3D-98-50-75');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (2,'2015-03-05 00:00:00','DV002','Verify card','Valid Card','3B-6E-6D-FD-85-76-A7');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (3,'2015-03-06 00:00:00','DV003','Verify card','Expired Card','3B-6E-6D-FD-85-76-A7');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (4,'2015-04-06 00:00:00','DV004','Verify card','Expired Card','C1-C5-D3-CF-A5-CA-2A');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (5,'2015-04-10 00:00:00','DV001','Verify card','Valid Card','0D-3B-D1-3D-98-50-75');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (6,'2015-05-01 00:00:00','DV003','Verify card','Valid Card','0D-3B-D1-3D-98-50-75');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (7,'2015-05-03 00:00:00','DV010','Verify card','Valid Card','0D-3B-D1-3D-98-50-75');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (8,'2015-05-06 00:00:00','DV011','Verify card','Valid Card','C1-C5-D3-CF-A5-CA-2A');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (9,'2015-05-07 00:00:00','DV005','Verify card','Expired Card','3B-6E-6D-FD-85-76-A7');
-INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_id`) VALUES (10,'2015-05-08 00:00:00','DV003','Verify card','Valid Card','45-FC-A2-C4-E6-F6-0E');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (1,'2015-03-04 00:00:00','DV001','Verify card','Valid Card','25');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (2,'2015-03-05 00:00:00','DV002','Verify card','Valid Card','25');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (3,'2015-03-06 00:00:00','DV003','Verify card','Expired Card','25');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (4,'2015-04-06 00:00:00','DV004','Verify card','Expired Card','25');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (5,'2015-04-10 00:00:00','DV001','Verify card','Valid Card','0D-3B-D1-3D-98-50-75');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (6,'2015-05-01 00:00:00','DV003','Verify card','Valid Card','0D-3B-D1-3D-98-50-75');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (7,'2015-05-03 00:00:00','DV010','Verify card','Valid Card','0D-3B-D1-3D-98-50-75');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (8,'2015-05-06 00:00:00','DV011','Verify card','Valid Card','C1-C5-D3-CF-A5-CA-2A');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (9,'2015-05-07 00:00:00','DV005','Verify card','Expired Card','3B-6E-6D-FD-85-76-A7');
+INSERT INTO `mic_card_access_log` (`id`,`access_date`,`device`,`request_service`,`response_content`,`card_instance_id`) VALUES (10,'2015-05-08 00:00:00','DV003','Verify card','Valid Card','45-FC-A2-C4-E6-F6-0E');
