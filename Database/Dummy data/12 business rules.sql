a7bea5b48339cc39bd3d6d228f4a6dfe3284582f
TriPQM <minhtridn2001@gmail.com>
2015-07-22 21:46:58 +0700

    Thêm Dummy data contract_renew_limit

--- Database/Dummy data/12 business rules.sql
+++ Database/Dummy data/12 business rules.sql
@@ -4,4 +4,4 @@ LIMIT 0, 1000
 
 -- Date: 2015-07-05 16:13
 */
-INSERT INTO `mic_business_rules` (`id`,`start_date_before`,`start_date_after`,`contract_min_term`,`contract_default_term`,`paid_date_before`,`paid_date_after`,`cancel_date_before`,`cancel_date_after`,`payment_due_date`,`nearly_exceed_expired_one`,`nearly_exceed_expired_two`,`nearly_exceed_expired_three`,`new_card_request_fee`,`delivery_fee`,`update_contract_due_date`) VALUES (1,7,1825,1,12,7,30,7,30,7,15,7,3,50000,10000,30);
+INSERT INTO `mic_business_rules` (`id`,`start_date_before`,`start_date_after`,`contract_min_term`,`contract_default_term`,`paid_date_before`,`paid_date_after`,`cancel_date_before`,`cancel_date_after`,`payment_due_date`,`nearly_exceed_expired_one`,`nearly_exceed_expired_two`,`nearly_exceed_expired_three`,`new_card_request_fee`,`delivery_fee`,`update_contract_due_date`,`contract_renew_limit`) VALUES (1,7,1825,1,12,7,30,7,30,7,15,7,3,50000,10000,30,60);
